ccfb618ea4d95e4fe5c130603c86f204c5e1f04e
Shiban Ashiq <shibanashiq743@gmail.com>
2023-08-30 13:43:54 +0530

    sdsd

--- lib/console_dart.dart
+++ lib/console_dart.dart
@@ -5,41 +5,37 @@ library console_dart;
 import 'dart:io';
 import 'package:args/args.dart';
 
-
 void load(List<String> arguments) {
   try {
-    
-  if (arguments.contains('--help')) {
-    print('''
+    if (arguments.contains('--help')) {
+      print('''
     Usage: create_file [options] <name> stl|stf
     Options:
       -c, --create-file    Create a file
       -s, --stl            Create a stateless widget
       -f, --stf            Create a statefull widget
     ''');
-    return;
-  }
-    
-  final results = ArgParser()
-    ..addOption('create-file', abbr: 'c')
-    ..addFlag('stl', abbr: 's')
-    ..addFlag('stf', abbr: 'f');
-    
-  final argResults = results.parse(arguments);
-  
-  final name = argResults['create-file'];
-  final stl = argResults['stl'];
-  final stf = argResults['stf'];
-  
- 
-  if (name != null) {
-    
-  print('Creating file $name');
-  
-  //publish stub file
-  
-  if (stl) {
-    const statelessStub = '''import 'package:flutter/material.dart';
+      return;
+    }
+
+    final results = ArgParser()
+      ..addOption('create-file', abbr: 'c')
+      ..addFlag('stl', abbr: 's')
+      ..addFlag('stf', abbr: 'f');
+
+    final argResults = results.parse(arguments);
+
+    final name = argResults['create-file'];
+    final stl = argResults['stl'];
+    final stf = argResults['stf'];
+
+    if (name != null) {
+      print('Creating file $name');
+
+      //publish stub file
+
+      if (stl) {
+        const statelessStub = '''import 'package:flutter/material.dart';
 
   class WIDGET extends StatelessWidget {
     const WIDGET ({super.key});
@@ -49,12 +45,11 @@ void load(List<String> arguments) {
       return Container();
     }
   }''';
-    
-    CreateFile.createFile(name, statelessStub);
-    print('Stateless widget created successfully PATH: $name');
-  }else if (stf) {
-    
-    const statefullStub = '''import 'package:flutter/material.dart';
+
+        CreateFile.createFile(name, statelessStub);
+        print('Stateless widget created successfully PATH: $name');
+      } else if (stf) {
+        const statefullStub = '''import 'package:flutter/material.dart';
 
 class WIDGET extends StatefulWidget {
   const WIDGET({super.key});
@@ -69,36 +64,32 @@ class _WIDGETState extends State<WIDGET> {
     return Container();
   }
 }''';
-    CreateFile.createFile(name, statefullStub);
-    print('Statefull widget created successfully PATH: $name');
-  }else{
-    print('Please provide a type of widget'); 
-  }
-  
-  }else{
-    print('Please provide a name for the file');
-  }
-  
-  }
-  catch (e) {
+        CreateFile.createFile(name, statefullStub);
+        print('Statefull widget created successfully PATH: $name');
+      } else {
+        print('Please provide a type of widget');
+      }
+    } else {
+      print('Please provide a name for the file');
+    }
+  } catch (e) {
     print(e);
   }
-  
 }
 
 class CreateFile {
   static void createFile(String name, stubs) {
     try {
-    final path = '${Directory.current.path}/lib/';
-    final file = File('$path$name.dart'); 
-    final className = name.split('/').last;
-    final classNameUpper = className[0].toUpperCase() + className.substring(1);
-    final stub = stubs.replaceAll('WIDGET', classNameUpper);
-    file.createSync(recursive: true);
-    file.writeAsStringSync(stub);
+      final path = '${Directory.current.path}/lib/';
+      final file = File('$path$name.dart');
+      final className = name.split('/').last;
+      final classNameUpper =
+          className[0].toUpperCase() + className.substring(1);
+      final stub = stubs.replaceAll('WIDGET', classNameUpper);
+      file.createSync(recursive: true);
+      file.writeAsStringSync(stub);
     } catch (e) {
       print(e);
     }
   }
 }
-
